e3d450a6a6b07b6da65e47a4797aaf2e8a484220
khw3268480 <khw3268480@naver.com>
2024-05-21 22:16:33 +0900

    First Commit

---
+++ src/main/java/org/canceldespawn/CancelDespawn.java
@@ -0,0 +1,17 @@
+package org.canceldespawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CancelDespawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
